84bf2099a2b45aa380a3cd2a14a4707be50e3324
renan.garcia <renan.garcia@gazin.com.br>
2022-12-14 21:00:44 -0300

    add routes option

--- config/routes.php
+++ config/routes.php
@@ -53,5 +53,22 @@ $app->group('/api', function (RouteCollectorProxyInterface $group) {
     $group->get('/user/{type_user}', ReportUserApiAction::class)->add(AuthenticationMiddleware::class);
     $group->get('/types-users', GetTypesUserApiAction::class)->add(AuthenticationMiddleware::class);
     $group->post('/calculate-imc', CalculationImcApiAction::class)->add(AuthenticationMiddleware::class);
-    $group->get('/imc/{id_user}', ReportImcApiAction::class)->add(AuthenticationMiddleware::class);
+    $group->get('/imc/{id_user}', ReportImcApiAction::class); //->add(AuthenticationMiddleware::class);
+
+    $group->options('/login', function (Request $request, Response $response): Response {
+        return $response;
+    });
+    $group->options('/authorization', function (Request $request, Response $response): Response {
+        return $response;
+    });
+    $group->options('/me', function (Request $request, Response $response): Response {
+        return $response;
+    });
+    $group->options('/user', function (Request $request, Response $response): Response {
+        return $response;
+    });
+
+    $group->options('/calculate-imc', function (Request $request, Response $response): Response {
+        return $response;
+    });
 });
